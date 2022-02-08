9c0940571ed7292dad1b40de74cbeaa3b47e7b8a
lenscas <lenscas@gmail.com>
2022-02-09 00:58:31 +0100

    enable more lua versions

--- test_cli/setup_cli_test.sh
+++ test_cli/setup_cli_test.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+VERSION_INFO=$(lua -v)
+FEATURE="lua54"
+
+if [[ "$VERSION_INFO" == *"5.3" ]]; then
+    FEATURE="lua53"
+elif [[ "$VERSION_INFO" == *"5.2" ]]; then
+    FEATURE="lua52"
+elif [[ "$VERSION_INFO" == *"5.1" ]]; then
+    FEATURE="lua51"
+else
+    FEATURE="lua54"
+fi
+
 cd pgteal_cli
-cargo build
+cargo build --features $FEATURE
 cd ../pgteal
-cargo build --lib
-cargo build --bin main
+cargo build --lib --features $FEATURE
+cargo build --bin main --features $FEATURE
 cd ../test_cli
 cp ../target/debug/libpgteal.so ./cli_test/libpgteal.so
 cp ../target/debug/libpgteal.d ./cli_test/libpgteal.d
