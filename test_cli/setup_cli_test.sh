fca8b0c2d9e378422372b72568509392f798551c
lenscas <lenscas@gmail.com>
2022-02-08 18:52:54 +0100

    fix permissions

--- test_cli/setup_cli_test.sh
+++ test_cli/setup_cli_test.sh
@@ -9,4 +9,4 @@ cd ../test_cli
 cp ../target/debug/libpgteal.so ./cli_test/libpgteal.so
 cp ../target/debug/libpgteal.d ./cli_test/libpgteal.d
 ../target/debug/main > ./cli_test/libpgteal.d.tl
-../target/debug/pgteal_cli --connection postgres://tealsql:tealsql@localhost/tealsql --sqlPattern cli_test/**/*.sql --tealPattern {dir}/{name}_{ext}.tl
+../target/debug/pgteal_cli --connection postgres://tealsql:tealsql@postgres/tealsql --sqlPattern cli_test/**/*.sql --tealPattern {dir}/{name}_{ext}.tl
