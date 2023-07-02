6d0eb1304e2d04a2f83d49a4abf883b6da1dc738
Lenscas <lenscas@gmail.com>
2023-07-03 00:45:57 +0200

    fix missing table in setup_db.sql

--- test_cli/setup_db.sql
+++ test_cli/setup_db.sql
@@ -10,3 +10,10 @@ CREATE TABLE everything (
 	interval1 interval NULL,
 	CONSTRAINT everything_pk PRIMARY KEY (varchar1)
 );
+
+CREATE TABLE public.testtable1 (
+	id serial4 NOT NULL,
+	name varchar NOT NULL,
+	CONSTRAINT testtable1_pk PRIMARY KEY (id)
+);
+CREATE INDEX testtable1_id_idx ON public.testtable1 USING btree (id);
